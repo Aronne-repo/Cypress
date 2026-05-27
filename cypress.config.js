ed337212a523dbf366ea4c58bf7ed53d8d874250
Aronne <285704982+Aronne-repo@users.noreply.github.com>
2026-05-27 16:14:55 +0200

    Initial setup of the project

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  retries: {
+    runMode: 2,
+    openMode: 0 // No retry when developing / debugging tests
+  },
+  e2e: {
+    baseUrl: 'https://opensource-demo.orangehrmlive.com',
+    viewportWidth: 1440,
+    viewportHeight: 900,
+    video: false,
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
+
+    setupNodeEvents(on, config) {
+      // Implement node event listeners here
+    },
+  },
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,10 @@
+Cypress.Commands.add('login', () => {
+  const username = Cypress.env('username')
+  const password = Cypress.env('password')
+
+  cy.visit('/web/index.php/auth/login')
+  cy.get('input[name="username"]').should('be.visible').type(username)
+  cy.get('input[name="password"]').should('be.visible').type(password)
+
+  cy.get('button[type="submit"]').click()
+})
